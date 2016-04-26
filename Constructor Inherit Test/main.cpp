61c675ee2d8c22df88d2bfc3565d62fd38232977
Kevin Xu Ke <lkstc112233@gmail.com>
2016-04-26 22:01:07 +0800

    Initial Commit

---
+++ Constructor Inherit Test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Constructor Inherit Test
+//
+//  Created by 煦 柯 on 16/4/26.
+//  Copyright © 2016年 Must Studio. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
